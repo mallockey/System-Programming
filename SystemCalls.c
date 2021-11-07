fd0ea88779232400159b39eecfc3848416407546
Josh Melo <joshuamelo1126@gmail.com>
2021-11-06 22:47:51 -0400

    file system work

--- SystemCalls.c
+++ SystemCalls.c
@@ -6,24 +6,61 @@
 #include <fcntl.h>
 #include <dirent.h>
 
-int main()
-{
-  // Attempting to print out directories
-  DIR *dirp = opendir("./test");  
-  struct dirent * dir = readdir(dirp);
+void writeToFile() {
 
-  // Write to file using System Calls
-  int file;
-  file = open("newfile.txt", O_RDWR | O_CREAT, 0750);
-             //Name of file, File Descriptors, Permissions
+  int fd, fd2, numOfReads;
+  char buffer[100];
 
-  if (file < 0) {
-    printf("Error!\n");
+  fd = open("./testfiles/RandomText.txt", O_RDWR | O_CREAT, 0777);
+  fd2 = open("./testfiles/modified_text.txt", O_RDWR | O_CREAT, 0777);
+
+  if ((fd | fd2) < 0) {
+    printf("Error opening file\n");
+  }
+
+  int readCount = read(fd, buffer, 100);
+
+  while(readCount > 0) {
+    //Logic for writing to modified_text.txt
+
+    write(fd2, buffer, 100);
+
+    readCount = read(fd, buffer, 1);
+    numOfReads++;
+  }
+
+  if (close(fd) | close(fd2) < 0) {
+    printf("Error closing the files");
+    exit(1);
+  }
+}
+
+void readFromFile() {
+  int fd;
+  char buffer[100];
+
+  fd = open("./testfiles/RandomText.txt", O_RDONLY, 0777);
+
+  if (fd < 0) {
+    printf("Error, reading file");
+    exit(1);
+  }
+
+  int readCount;
+  readCount = read(fd, buffer, 100);
+
+  if (readCount < 0) {
+    printf("Error");
     exit(1);
   } else {
-    char *outMessage = "We have written to the system correctly!\n";
-    // Print to console with system calls
-    write(STDOUT_FILENO, outMessage, strlen(outMessage));
+    printf("%i", readCount);
+    printf("%.*s\n", readCount, buffer);
   }
+}
+
+int main()
+{
+  writeToFile();
+  readFromFile();
   return 0;
 }
